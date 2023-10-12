e43d88230af838083fc555984fdda7abb27d3042
Kathan Thakkar <thakkarkathan1904@gamil.com>
2023-10-13 01:38:15 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express()
 const port = 3000
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO);
+}
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
