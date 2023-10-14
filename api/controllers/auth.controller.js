75d20b1f586155db7bff179b1f14a53cf89a335a
Kathan Thakkar <thakkarkathan1904@gamil.com>
2023-10-14 21:16:43 +0530

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,15 @@
 const User = require("../models/user.model.js");
-const bcryptjs = require('bcryptjs');
-const signup = async(req,res)=>{
-    const { username, email, password } = req.body;
-    const hashedpassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password:hashedpassword });
-    try {
-        await newUser.save();
-        res.status(201).json("User created successfully");
-    } catch (error) {
-        res.status(500).json(error.message);
-    }
-    
+const bcryptjs = require("bcryptjs");
+const errorHandler = require("../utils/error.js");
+const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedpassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedpassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
 };
 module.exports = signup;
--- api/index.js
+++ api/index.js
@@ -15,12 +15,18 @@ async function main() {
   await mongoose.connect(process.env.MONGO);
 }
 
-
-
-app.use("/api/user",userRoute);
-app.use("/api/auth",authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
