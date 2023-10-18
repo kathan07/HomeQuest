ada78d4e71984067b0815e4ee2ec737f228ea20d
Kathan Thakkar <thakkarkathan1904@gamil.com>
2023-10-18 17:32:51 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const User = require("../models/user.model.js");
 const bcryptjs = require("bcryptjs");
 const errorHandler = require("../utils/error.js");
+const jwt = require("jsonwebtoken");
+
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedpassword = bcryptjs.hashSync(password, 10);
@@ -12,4 +14,25 @@ const signup = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = signup;
+
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = await bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password: pass,...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = {signup, signin};
