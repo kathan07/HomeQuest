eb0b40de69ea81b85b56c1b0662ccc47033c72cf
kathan07 <thakkarkathan1904@gmail.com>
2023-12-24 01:21:06 +0530

    Listing route problem fixed

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const verifyToken = require("../utils/verifyUser");
-const { createListing } = require('../controllers/listing.controller.js');
+const createListing = require("../controllers/listing.controller.js");
+
 
 const router = express.Router();
 
