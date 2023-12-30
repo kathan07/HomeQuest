fac897a454b7a8fee905427d905a76cecbf26e3d
kathan07 <thakkarkathan1904@gmail.com>
2023-12-30 17:53:18 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,6 @@
 const Listing = require("../models/listing.model.js");
+const errorHandler = require("../utils/error.js");
+
 
 const createListing = async (req, res, next) => {
   try {
@@ -27,4 +29,26 @@ const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = { createListing, deleteListing };
+
+
+const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only update your own listings!'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = { createListing, deleteListing,  updateListing};
