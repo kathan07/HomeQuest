4439395bf4900e9fb0d3d4070d8042d38a0f9835
kathan07 <thakkarkathan1904@gmail.com>
2024-05-27 21:04:10 +0530

    Theme changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,30 +5,30 @@ export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   return (
     <>
-      <header className='bg-slate-200 shadow-md'>
+      <header className='bg-violet-600 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto  p-3'>
           <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-              <span className='text-slate-500'>Real</span>
-              <span className='text-slate-700'>Estate</span>
+              <span className='text-violet-100'>Real</span>
+              <span className='text-violet-950'>Estate</span>
             </h1>
           </Link>
-          <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <form className='bg-violet-200 p-3 rounded-lg flex items-center'>
             <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-            <FaSearch className='text-slate-600' />
+            <FaSearch className='text-violet-600' />
           </form>
           <ul className='flex gap-4'>
             <Link to='/Home'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+              <li className='hidden sm:inline text-violet-100 hover:underline'>Home</li>
             </Link>
             <Link to='/About'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+              <li className='hidden sm:inline text-violet-100 hover:underline'>About</li>
             </Link>
             <Link to='/profile'>
               {currentUser ? (
                 <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
               ) : (
-                <li className=' text-slate-700 hover:underline'> Sign in</li>
+                <li className=' text-violet-100 hover:underline'> Sign in</li>
               )}
             </Link>
           </ul>
